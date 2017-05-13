c71f8841fa75c959161acbbcb91fae081f2143ea
Tanawut023 <s5730213023@phuket.psu.ac.th>
2017-05-13 16:59:20 +0700

    edit add supplier

--- project1/instrument_add.php
+++ project1/instrument_add.php
@@ -11,7 +11,7 @@ if($_POST){
 	$brand = $_POST['brand'];
 	$status = "no";
 
-	$sql = "INSERT INTO suppliers (sup_name,address,phone,sup_password,sub_accname,sub_email,sup_accnum,status,sub_brand) VALUES ('$name','$address','$phone','$password','$accname','$email','$accnum','$status','$brand')";
+	$sql = "INSERT INTO suppliers (sup_name,address,phone,sup_password,sub_accname,sub_email,sup_accnum,status,sub_brand) VALUES ('$name','$address','$tel','$password','$accname','$email','$accnum','$status','$brand')";
 	mysqli_query($link,$sql);
 	$sup_id = mysqli_insert_id($link);
 	if(is_uploaded_file($_FILES['file']['tmp_name'])){
